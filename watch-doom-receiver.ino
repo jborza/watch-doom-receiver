5327cfc526ae8914378e4692c6e379295f00e528
jborza <jborza@gmail.com>
2020-11-10 23:43:13 +0100

    receiving 240x240 data

--- watch-doom-receiver.ino
+++ watch-doom-receiver.ino
@@ -5,10 +5,9 @@ TFT_eSPI *tft;
 
 #define INVERT_COLORS
 
-
 #define DISPLAY_LINE_LENGTH 240
-#define RECEIVE_LINE_BITS 120
-#define RECEIVE_LINE_BYTES 120/8
+#define RECEIVE_LINE_BITS 240
+#define RECEIVE_LINE_BYTES 240/8
 #define BAUD_RATE 500000
 #define HSYNC 0x48
 #define VSYNC 0x56
@@ -16,7 +15,7 @@ TFT_eSPI *tft;
 int y = 0;
 int Y_MAX = 240;
 uint8_t rxBuffer[RECEIVE_LINE_BYTES];
-uint32_t lineBuffer[DISPLAY_LINE_LENGTH/2];
+uint16_t lineBuffer[DISPLAY_LINE_LENGTH];
 
 void setup() {
 
@@ -42,6 +41,8 @@ void setup() {
 
 //now let's do 2x2 scaling
 void loop() {
+  uint8_t rx;
+  uint8_t idx;
   //pixel doubling loop
   //receive 120 pixels (120 bits = 15 bytes) over serial port
   //then push two rows of pixels to the screen
@@ -51,30 +52,25 @@ void loop() {
     Serial.write(VSYNC);
     return;
   }
-  tft->setAddrWindow(0, y, DISPLAY_LINE_LENGTH, 2);
-    //convert 120 received bits to 240 line pixels
-    //convert my rxBuffer to black and white pixels
-
-  int bit = 7;
-  for (int i = 0; i < RECEIVE_LINE_BITS; i++) {
-    //bit 0 -> pixel 0,1; bit 1 -> pixel 1,2
-    //int remainder = 7 - i % 8;
-#ifdef INVERT_COLORS
-    int color = (((rxBuffer[i >> 3] >> (bit)) & 1) == 1 ? 0xFFFFFFFF : 0);
-#else
-    int color = (((rxBuffer[i / 8] >> (remainder)) & 1) == 1 ? 0 : 0xFFFF);
-#endif
-    lineBuffer[i] = color;
-    if (bit == 0)
-      bit = 7;
-        else
-        bit--;
-  }
+  tft->setAddrWindow(0, y, DISPLAY_LINE_LENGTH, 1);
+    //convert my rxBuffer (240 bits = 30 bytes) to 240 black and white pixels
+    for(int x = 0; x < RECEIVE_LINE_BYTES; x++){
+      rx = rxBuffer[x];
+      idx = x << 3;
+      lineBuffer[idx] = (((rx >> 7) & 1) == 1 ? 0xFFFF : 0);
+      lineBuffer[idx+1] = (((rx >> 6) & 1) == 1 ?  0xFFFF : 0);
+      lineBuffer[idx+2] = (((rx >> 5) & 1) == 1 ?  0xFFFF : 0);
+      lineBuffer[idx+3] = (((rx >> 4) & 1) == 1 ?  0xFFFF : 0);
+      lineBuffer[idx+4] = (((rx >> 3) & 1) == 1 ?  0xFFFF : 0);
+      lineBuffer[idx+5] = (((rx >> 2) & 1) == 1 ?  0xFFFF : 0);
+      lineBuffer[idx+6] = (((rx >> 1) & 1) == 1 ?  0xFFFF : 0);
+      lineBuffer[idx+7] = (((rx) & 1) == 1 ?  0xFFFF : 0);
+   }
+  
   tft->startWrite();
   tft->pushPixels(lineBuffer, DISPLAY_LINE_LENGTH);
-  tft->pushPixels(lineBuffer, DISPLAY_LINE_LENGTH);
   tft->endWrite();
-  y+=2;
+  y+=1;
   if(y >= Y_MAX){
     y = 0;    
     Serial.write(VSYNC);
