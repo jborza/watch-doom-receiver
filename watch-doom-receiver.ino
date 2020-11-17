6af5deedce6c7d737f13c81867f8d28afb857f80
jborza <jborza@gmail.com>
2020-11-17 10:11:23 +0100

    watch receiver color schemes

--- watch-doom-receiver.ino
+++ watch-doom-receiver.ino
@@ -3,8 +3,6 @@
 TTGOClass *ttgo;
 TFT_eSPI *tft;
 
-//#define INVERT_COLORS
-
 #define DISPLAY_LINE_LENGTH 240
 #define RECEIVE_LINE_BITS 240
 #define RECEIVE_LINE_BYTES 240/8
@@ -12,6 +10,24 @@ TFT_eSPI *tft;
 #define HSYNC 0x48
 #define VSYNC 0x56
 #define COLOR 0x07E0
+#define RED 0x00F8
+#define BLUE 0xF800
+#define BLACK 0x0
+#define WHITE 0xFFFF
+#define PURPLE 0x07E0
+
+//scheme 0: white, black
+//#define COLOR_LIGHT WHITE
+//#define COLOR_DARK BLACK
+//scheme 1: blue, black: 0xF800 + 0xFFFF
+//#define COLOR_LIGHT BLUE
+//#define COLOR_DARK BLACK
+//scheme 2: white, blue: 0xF800 + 0xFFFF
+//#define COLOR_LIGHT WHITE
+//#define COLOR_DARK BLUE
+//scheme 3: white, purple
+#define COLOR_LIGHT WHITE
+#define COLOR_DARK PURPLE
 
 int y = 0;
 int Y_MAX = 240;
@@ -39,7 +55,6 @@ void setup() {
   delay(2000);
 }
 
-
 //now let's do 2x2 scaling
 void loop() {
   uint8_t rx;
@@ -55,28 +70,18 @@ void loop() {
   }
   tft->setAddrWindow(0, y, DISPLAY_LINE_LENGTH, 1);
     //convert my rxBuffer (240 bits = 30 bytes) to 240 black and white pixels
+    uint16_t color = 0x00F8;
     for(int x = 0; x < RECEIVE_LINE_BYTES; x++){
       rx = rxBuffer[x];
       idx = x << 3;
-      #ifdef INVERT_COLORS
-      lineBuffer[idx] = (((rx >> 7) & 1) == 1 ? 0 : COLOR);
-      lineBuffer[idx+1] = (((rx >> 6) & 1) == 1 ?  0: COLOR);
-      lineBuffer[idx+2] = (((rx >> 5) & 1) == 1 ?  0:COLOR);
-      lineBuffer[idx+3] = (((rx >> 4) & 1) == 1 ? 0: COLOR);
-      lineBuffer[idx+4] = (((rx >> 3) & 1) == 1 ? 0 : COLOR);
-      lineBuffer[idx+5] = (((rx >> 2) & 1) == 1 ? 0 : COLOR);
-      lineBuffer[idx+6] = (((rx >> 1) & 1) == 1 ? 0 : COLOR);
-      lineBuffer[idx+7] = (((rx) & 1) == 1 ? 0 : COLOR );
-      #else
-      lineBuffer[idx] = (((rx >> 7) & 1) == 1 ? COLOR : 0);
-      lineBuffer[idx+1] = (((rx >> 6) & 1) == 1 ?  COLOR : 0);
-      lineBuffer[idx+2] = (((rx >> 5) & 1) == 1 ?  COLOR : 0);
-      lineBuffer[idx+3] = (((rx >> 4) & 1) == 1 ?  COLOR : 0);
-      lineBuffer[idx+4] = (((rx >> 3) & 1) == 1 ?  COLOR : 0);
-      lineBuffer[idx+5] = (((rx >> 2) & 1) == 1 ?  COLOR : 0);
-      lineBuffer[idx+6] = (((rx >> 1) & 1) == 1 ?  COLOR : 0);
-      lineBuffer[idx+7] = (((rx) & 1) == 1 ?  COLOR : 0);
-      #endif
+      lineBuffer[idx] = (((rx >> 7) & 1) == 1   ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+1] = (((rx >> 6) & 1) == 1 ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+2] = (((rx >> 5) & 1) == 1 ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+3] = (((rx >> 4) & 1) == 1 ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+4] = (((rx >> 3) & 1) == 1 ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+5] = (((rx >> 2) & 1) == 1 ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+6] = (((rx >> 1) & 1) == 1 ? COLOR_LIGHT : COLOR_DARK);
+      lineBuffer[idx+7] = (((rx) & 1) == 1      ? COLOR_LIGHT : COLOR_DARK);
    }
   
   tft->startWrite();
